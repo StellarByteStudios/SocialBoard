aa66405243df8493f5db26dfaf2073daa39558c0
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-03 17:20:00 +0100

    Neue Controller angelegt

--- src/main/java/stellarbytestudios/socialboard/controller/PathLibary.java
+++ src/main/java/stellarbytestudios/socialboard/controller/PathLibary.java
@@ -1,5 +1,18 @@
 package stellarbytestudios.socialboard.controller;
 
 public class PathLibary {
+
+    //Hauptseite
     public static final String STARTPAGE = "/";
+
+    //Registrierungskram
+    public static final String REGISTRIERUNG = "/registrierung";
+
+    //Anmeldungskram
+    public static final String ANMELDUNG = "/anmeldung";
+
+    //Userseite
+    public static final String LOADUSERPAGE = "/personalfeed";
+
+
 }
--- src/main/java/stellarbytestudios/socialboard/controller/RegistrierungController.java
+++ src/main/java/stellarbytestudios/socialboard/controller/RegistrierungController.java
@@ -1,18 +1,11 @@
 package stellarbytestudios.socialboard.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static stellarbytestudios.socialboard.controller.PathLibary.*;
 
 @Controller
-@RequestMapping(LOADUSERPAGE)
-public class UserPageConntroller {
-
-    public String loadPersonalFeed(Model m){
-
-
-        return
-    }
+@RequestMapping(REGISTRIERUNG)
+public class RegistrierungController {
 }
