b50adf31b4d0b516d12768fbdc83fe6e41d17ca7
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-03 14:13:58 +0100

    Introduced Packages

--- src/main/java/stellarbytestudios/socialboard/controller/StartPageController.java
+++ src/main/java/stellarbytestudios/socialboard/controller/StartPageController.java
@@ -1,4 +1,4 @@
-package stellarbytestudios.socialboard;
+package stellarbytestudios.socialboard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
