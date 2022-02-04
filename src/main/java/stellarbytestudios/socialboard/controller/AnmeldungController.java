7ffa3954c900bd1e6bc3c6bb069cd910053e6f3e
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-04 16:37:03 +0100

    Refactoring of User-Record

--- src/main/java/stellarbytestudios/socialboard/controller/AnmeldungController.java
+++ src/main/java/stellarbytestudios/socialboard/controller/AnmeldungController.java
@@ -3,10 +3,9 @@ package stellarbytestudios.socialboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import stellarbytestudios.socialboard.core.User;
+import stellarbytestudios.socialboard.core.UserRec;
 
 import static stellarbytestudios.socialboard.controller.PathLibary.*;
 
@@ -19,9 +18,9 @@ public class AnmeldungController {
     @GetMapping(VERIFYUSER)
     public String verify(Model m, RedirectAttributes redirectAttributes, String name, String password){
         //Dummydaten
-        User user = new User(0, "The First One", "supadupasecret");
-        redirectAttributes.addAttribute("username", user.username());
-        redirectAttributes.addAttribute("password", user.password());
+        UserRec userRec = new UserRec(0, "The First One", "supadupasecret");
+        redirectAttributes.addAttribute("username", userRec.username());
+        redirectAttributes.addAttribute("password", userRec.password());
         System.out.println("User wurde verifiziert");
         return "redirect:" + USERCONTROLLER + USERFEED;
     }
---
+++ src/main/java/stellarbytestudios/socialboard/core/UserRec.java
@@ -0,0 +1,4 @@
+package stellarbytestudios.socialboard.core;
+
+public record UserRec(int userID, String username, String password) {
+}
