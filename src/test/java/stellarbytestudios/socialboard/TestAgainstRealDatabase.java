30e777e4a7eab7526a5c53bfa8e0f282aaceadf0
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-05 14:46:22 +0100

    Refactor UserRepo

--- src/test/java/stellarbytestudios/socialboard/TestAgainstRealDatabase.java
+++ src/test/java/stellarbytestudios/socialboard/TestAgainstRealDatabase.java
@@ -3,9 +3,7 @@ package stellarbytestudios.socialboard;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import stellarbytestudios.socialboard.core.UserRec;
-import stellarbytestudios.socialboard.database.DBcommunication.UserRepo;
+import stellarbytestudios.socialboard.database.DBcommunication.UserCrudRepo;
 import stellarbytestudios.socialboard.database.DTOs.DropDTO;
 import stellarbytestudios.socialboard.database.DTOs.UserDTO;
 import stellarbytestudios.socialboard.services.UserServiceRepository;
@@ -18,7 +16,7 @@ import java.util.Set;
 public class TestAgainstRealDatabase {
 
     @Autowired
-    private UserRepo userRepo;
+    private UserCrudRepo userCrudRepo;
 
     @Autowired
     private UserServiceRepository serviceRepository;
@@ -37,52 +35,52 @@ public class TestAgainstRealDatabase {
                 LocalDateTime.now().minusHours(5)));
 
         UserDTO user1 = new UserDTO(null, "The first One", "supadupaSecred", drops);
-        userRepo.save(user1);
+        userCrudRepo.save(user1);
         System.out.println("Einfachen User mit 3 Drops hinzufügen");
-        System.out.println(userRepo.findAll());
+        System.out.println(userCrudRepo.findAll());
         System.out.println();
 
         // Weiteren Drop hinzufügen
         user1.addDrop(new DropDTO("Der Nachkömmling",
                 LocalDateTime.now()));
-        userRepo.save(user1);
+        userCrudRepo.save(user1);
         System.out.println("Weiteren Drop hinzufügen");
-        System.out.println(userRepo.findAll());
+        System.out.println(userCrudRepo.findAll());
         System.out.println();
 
 
         // Weiteren User ohne Drops hinzufügen
         UserDTO user2 = UserDTO.create(null, "Schüler1", "123");
-        userRepo.save(user2);
+        userCrudRepo.save(user2);
         System.out.println("Weiteren User ohne Drops hinzufügen");
-        System.out.println(userRepo.findAll());
+        System.out.println(userCrudRepo.findAll());
         System.out.println();
 
         // Dieser Droped was
         DropDTO dropUser2 = new DropDTO("Legen   ...   dary", LocalDateTime.now());
         user2.addDrop(dropUser2);
-        userRepo.save(user2);
+        userCrudRepo.save(user2);
         System.out.println("Dieser Droped was");
-        System.out.println(userRepo.findAll());
+        System.out.println(userCrudRepo.findAll());
         System.out.println();
 
         // Jetzt Löscht der Erste einen Eintrag
         user1.removeDropByContent("This is eins nice Drop");
-        userRepo.save(user1);
+        userCrudRepo.save(user1);
         System.out.println("Jetzt Löscht der Erste einen Eintrag (Den ersten)");
-        System.out.println(userRepo.findAll());
+        System.out.println(userCrudRepo.findAll());
         System.out.println();
 
         // Jetzt Ändert der Zweite seinen Eintrag
         user2.changeDropcontent("Legen   ...   dary", "MOOOOR LEGENNNNDAAAARY");
-        userRepo.save(user2);
+        userCrudRepo.save(user2);
         System.out.println("Jetzt Ändert der Zweite seinen Eintrag");
-        System.out.println(userRepo.findAll());
+        System.out.println(userCrudRepo.findAll());
         System.out.println();
 
         // Versuche einen Nutzer durch seinen Namen zu finden
         System.out.println("Versuche einen Nutzer durch seinen Namen zu finden");
-        System.out.println(userRepo.findUserDTOByUsername("Schüler1"));
+        System.out.println(userCrudRepo.findUserDTOByUsername("Schüler1"));
         System.out.println();
 
 //        // Passworttest
@@ -96,9 +94,9 @@ public class TestAgainstRealDatabase {
 
 
         // Alle Testdaten löschen
-        userRepo.deleteAll();
+        userCrudRepo.deleteAll();
         System.out.println("Alle Testdaten löschen");
-        System.out.println(userRepo.findAll());
+        System.out.println(userCrudRepo.findAll());
         System.out.println();
     }
 
