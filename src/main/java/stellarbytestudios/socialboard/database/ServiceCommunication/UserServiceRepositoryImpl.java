405afd455bd68a2d03ecfad8c32ba6f31b7fe214
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-05 14:47:26 +0100

    Schittstelle von der Datenbank zum Service

--- src/main/java/stellarbytestudios/socialboard/database/ServiceCommunication/UserServiceRepositoryImpl.java
+++ src/main/java/stellarbytestudios/socialboard/database/ServiceCommunication/UserServiceRepositoryImpl.java
@@ -1,11 +1,26 @@
 package stellarbytestudios.socialboard.database.ServiceCommunication;
 
 import org.springframework.stereotype.Repository;
-import stellarbytestudios.socialboard.database.DBcommunication.UserRepo;
+import stellarbytestudios.socialboard.core.UserRec;
+import stellarbytestudios.socialboard.database.DBcommunication.UserCrudRepo;
+import stellarbytestudios.socialboard.database.DTOs.UserDTO;
 import stellarbytestudios.socialboard.services.UserServiceRepository;
 
+
+// Diese Klasse Implementiert das Repository aus dem Service Package
+// Es ist zusammen mit diesem Interface die Schnittstelle zwischen Services und
+// der Datenbank
 @Repository
 public class UserServiceRepositoryImpl implements UserServiceRepository {
 
-    UserRepo userRepo;
+    // Verbindung zur Datenbank
+    UserCrudRepo userCrudRepo;
+
+    // Überprüft ob der übergebene User so in der Datenbank steht (Name und Passwort korrekt)
+    @Override
+    public boolean validateUserLogin(UserRec user) {
+        UserDTO userInDb = userCrudRepo.findUserDTOByUsername(user.username());
+        if (userInDb == null) { return false; }
+        return userInDb.equalsWithRecord(user);
+    }
 }
