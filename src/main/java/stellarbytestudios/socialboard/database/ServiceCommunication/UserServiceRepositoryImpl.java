6ac225689310d60b511c78eccd8bb3e62678d0ab
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-07 16:33:05 +0100

    UserServiceRepository: usernameAlreadyTaken
    Man kann jetzt in der Datenbank nachschauen, ob es bereits einen Nutzer gibt der mit diesem Namen registriert ist

--- src/main/java/stellarbytestudios/socialboard/database/ServiceCommunication/UserServiceRepositoryImpl.java
+++ src/main/java/stellarbytestudios/socialboard/database/ServiceCommunication/UserServiceRepositoryImpl.java
@@ -6,6 +6,9 @@ import stellarbytestudios.socialboard.database.DBcommunication.UserCrudRepo;
 import stellarbytestudios.socialboard.database.DTOs.UserDTO;
 import stellarbytestudios.socialboard.services.UserServiceRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 // Diese Klasse Implementiert das Repository aus dem Service Package
 // Es ist zusammen mit diesem Interface die Schnittstelle zwischen Services und
@@ -27,5 +30,19 @@ public class UserServiceRepositoryImpl implements UserServiceRepository {
         UserDTO userInDb = userCrudRepo.findUserDTOByUsername(user.username());
         if (userInDb == null) { return false; }
         return userInDb.equalsWithRecord(user);
+    }
+
+    // Überprüft, ob ein Nutzer mit diesem Namen bereits in der Datenbank ist
+    @Override
+    public boolean usernameAlreadyTaken(String username) {
+        // Alle Nutzer aus der Datenbank holen
+        Iterable<UserDTO> usersIterable = userCrudRepo.findAll();
+        // Daten zur einer Liste verarbeiten, sodass man sie vernünftig benuzten kann
+        // Brauche nur die Namen zum abgleichen
+        List<String> usernamesList = new ArrayList<>();
+        usersIterable.forEach(userDTO -> {usernamesList.add(userDTO.getUsername());});
+
+        // Den Namen suchen, ob er schon vorhanden ist
+        return usernamesList.contains(username);
     }
 }
