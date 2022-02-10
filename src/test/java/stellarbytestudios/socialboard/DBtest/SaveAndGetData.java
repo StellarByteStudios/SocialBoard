e746bf30baeac7f70bfa5019d04d82f4aa2e01ce
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-10 17:03:51 +0100

    DB Testing - Klasse für die Tests erstellt
    Klasse für die Datenbanktests erstellt und man hin geschrieben, was alles getestet werden muss

--- src/test/java/stellarbytestudios/socialboard/DBtest/SaveAndGetData.java
+++ src/test/java/stellarbytestudios/socialboard/DBtest/SaveAndGetData.java
@@ -1,7 +1,13 @@
 package stellarbytestudios.socialboard.DBtest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.test.context.ActiveProfiles;
+import stellarbytestudios.socialboard.database.DBcommunication.UserCrudRepo;
+import stellarbytestudios.socialboard.database.ServiceCommunication.UserServiceRepositoryImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,5 +15,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("test")
 public class SaveAndGetData {
 
-   // Jep, hier lässt sich nichts Testen ka warum
+    // Initialisierung
+    @Autowired
+    UserCrudRepo dataBase;
+
+    UserServiceRepositoryImpl repo;
+
+    @BeforeEach
+    public void setUp(){
+        repo = new UserServiceRepositoryImpl(dataBase);
+    }
+
+    // User aus der Datenbank lesen
+    @Test
+    @DisplayName("User aus der Datenbank lesen")
+    public void readUser() {
+
+        assertThat(true).isTrue();
+    }
+    // Nicht vorhandener User suchen
+
+    // Drops aus der Datenbank holen
+
+    // Neuen Nutzer anlegen (Ohne Drops)
+
+    // Drops hinzufügen
+
+    // Drops bearbeiten
+
+    // Drop Löschen
 }
