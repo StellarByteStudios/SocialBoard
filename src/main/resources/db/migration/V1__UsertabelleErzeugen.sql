998b7a951ab23f196ab02d26da66041e7e9abd49
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-10 17:02:48 +0100

    DB - Erstellung der Usertabelle angepasst
    Die SQL Syntax hat leider nicht für die h2 Datenbank funktioniert, sondern nur für MariaDB. Syntax wurde jetzt so angepasst, sodass beide sie verstehen

--- src/main/resources/db/migration/V1__UsertabelleErzeugen.sql
+++ src/main/resources/db/migration/V1__UsertabelleErzeugen.sql
@@ -2,21 +2,15 @@
 create table Users
 (
     -- userId ist der Primärschlüssel --
-	userID int,
+	userID int not null auto_increment,
 	-- Daten des Users --
 	username varchar(100) default 'defaultusername' not null,
 	userpasswordsalt int default 123456789 not null,
 	userpasswordhash varchar(130) default 'someHash' not null,
+	-- Macht die ID zum Primärschlüssel --
+	constraint Users_pk primary key (userID)
 );
 
 -- Macht die ID des User einzigartig (keine doppelten IDs) --
 create unique index Users_userID_uindex
 	on Users (userID);
-
--- Macht die ID zum Primärschlüssel --
-alter table Users
-	add constraint Users_pk
-		primary key (userID);
-
--- Die ID wird bei neuen Usern automatisch hochgezählt --
-alter table Users modify userID int auto_increment;
