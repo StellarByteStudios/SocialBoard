00e40fd157c7e7f4c71d8c3ddca579460fc1537a
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-18 11:25:09 +0100

    Anzeige der Drops 3: Drop-Record Optimazitaion
    Drop-Record wurde für die Anzeige Optimiert. Es ist ein Feld für den Autor dazugekommen. Zusätzlich wurde noch eine Hilfsmethode hinzugefügt, dass die Drops nach Erstellungsdatum sortiert werden können

--- src/main/java/stellarbytestudios/socialboard/core/DropRec.java
+++ src/main/java/stellarbytestudios/socialboard/core/DropRec.java
@@ -2,5 +2,9 @@ package stellarbytestudios.socialboard.core;
 
 import java.time.LocalDateTime;
 
-public record DropRec(String content, LocalDateTime creationDate) {
+public record DropRec(String author, String content, LocalDateTime creationDate) {
+
+    public static int compareDate(DropRec dropOne, DropRec dropTwo) {
+        return dropOne.creationDate.compareTo(dropTwo.creationDate) * -1;
+    }
 }
