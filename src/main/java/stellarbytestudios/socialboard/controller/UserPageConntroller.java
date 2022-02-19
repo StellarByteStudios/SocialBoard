ea409df6a7629acc12c7142acc4e73a2798de304
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-19 09:46:17 +0100

    Posting Drop without Username
    Jetzt wird auch nicht nur nicht beim Redirect der Name in der URL mit übertragen, sondern auch nicht mehr, wenn man mit einem Post-Request einen Drop abschickt. Wird jetzt über das Formular gelöst

--- src/main/java/stellarbytestudios/socialboard/controller/UserPageConntroller.java
+++ src/main/java/stellarbytestudios/socialboard/controller/UserPageConntroller.java
@@ -36,8 +36,8 @@ public class UserPageConntroller {
         return "userpage";
     }
 
-    @PostMapping(NEWDROP + "/{username}")
-    public String userDropsSomething(@PathVariable String username, String dropcontent, RedirectAttributes readds){
+    @PostMapping(NEWDROP)
+    public String userDropsSomething(String username, String dropcontent, RedirectAttributes readds){
 
         // Neuen Drop abspeichern
         userService.saveNewDrop(username, dropcontent);
