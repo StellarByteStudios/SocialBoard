a034a4b34f4fba52da02d5fee0ac1876ccf6a843
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-02-26 12:28:23 +0100

    Commentary of Repository-Implementation

--- src/main/java/stellarbytestudios/socialboard/database/ServiceCommunication/DropsHandlingRepositoryImpl.java
+++ src/main/java/stellarbytestudios/socialboard/database/ServiceCommunication/DropsHandlingRepositoryImpl.java
@@ -11,6 +11,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+// Diese Klasse Implementiert das Repository aus dem Service Package
+// Es ist zusammen mit diesem Interface die Schnittstelle zwischen Services und
+// der Datenbank
 @Repository
 public class DropsHandlingRepositoryImpl implements DropsHandlingRepository {
 
@@ -22,6 +25,7 @@ public class DropsHandlingRepositoryImpl implements DropsHandlingRepository {
         this.userCrudRepo = userCrudRepo;
     }
 
+    // holt alle Drops aus der Datenbank für den Globalfeed
     @Override
     public List<DropRec> getAllDrops() {
         // Alle User aus der Datenbank holen
@@ -31,18 +35,23 @@ public class DropsHandlingRepositoryImpl implements DropsHandlingRepository {
 
         userDTOIterable.forEach(user ->     // Für jeden User
                 user.getDropDTOS().         // Hol ich mir seine Drops
-                        forEach(dropDTO     // Und füg sie alle der Dropliste hinzu
-                                -> drops.add(mapDropDTOtoRecord(user.getUsername(),dropDTO))));
+                        forEach(dropDTO ->  // Und füg sie alle der Dropliste hinzu
+                            drops.add(mapDropDTOtoRecord(user.getUsername(),dropDTO))));
         return drops;
     }
 
+    // Speichert einen neuen Drop in der Datenbank
     @Override
     public void saveNewDrop(String username, String dropcontent) {
+        // Postenden User aus der Datenbank holen
         UserDTO user = userCrudRepo.findUserDTOByUsername(username);
+        // Bei diesem einen neuen Drop hinzufügen
         user.addDrop(new DropDTO(dropcontent, LocalDateTime.now()));
+        // Aktuallisierten User abspeichern
         userCrudRepo.save(user);
     }
 
+    // Mapper der den Username und ein DropDTO annimmt und daraus ein Record macht
     private static DropRec mapDropDTOtoRecord(String username, DropDTO dropDTO){
         DropRec record = new DropRec(username,dropDTO.getContent(), dropDTO.getDateOfWriting());
         return record;
