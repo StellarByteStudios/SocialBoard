f564a7a7b5233f3d741cb63b4a823b05fb35c5fb
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-03-01 13:46:30 +0100

    Reworked migration-Skripts
    Im Skript muss nicht alles in Caps-Lock geschrieben sein. Der Einfachheit lass ich es "normal"

--- src/main/resources/db/migration/V1__UsertabelleErzeugen.sql
+++ src/main/resources/db/migration/V1__UsertabelleErzeugen.sql
@@ -1,16 +1,12 @@
 -- Erzeugen der Tabelle in der die Userdaten gespeichert werden --
-create table USERS
+create table Users
 (
     -- userId ist der Primärschlüssel --
-	USERID int not null auto_increment,
+	userID int not null auto_increment,
 	-- Daten des Users --
-	USERNAME varchar(100) default 'defaultusername' not null,
-	USERPASSWORDSALT int default 123456789 not null,
-	USERPASSWORDHASH varchar(130) default 'someHash' not null,
+	username varchar(100) default 'defaultusername' not null,
+	userpasswordsalt int default 123456789 not null,
+	userpasswordhash varchar(130) default 'someHash' not null,
 	-- Macht die ID zum Primärschlüssel --
-	constraint USERS_pk primary key (userID)
+	constraint Users_pk primary key (userID)
 );
---
--- -- Macht die ID des User einzigartig (keine doppelten IDs) --
--- create unique index USERS_USERID_uindex
--- 	on USERS (userID);
