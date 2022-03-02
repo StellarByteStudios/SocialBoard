7338b8731061ee82569a370f1308a1aef2111640
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-03-02 17:22:59 +0100

    Cleaning imports of StructureTest

--- src/test/java/stellarbytestudios/socialboard/architechture/StructureTest.java
+++ src/test/java/stellarbytestudios/socialboard/architechture/StructureTest.java
@@ -1,19 +1,13 @@
 package stellarbytestudios.socialboard.architechture;
 
-import com.tngtech.archunit.base.PackageMatchers;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import org.junit.jupiter.api.Test;
 import stellarbytestudios.socialboard.SocialBoardApplication;
 
-import java.lang.annotation.Annotation;
-import java.util.HashSet;
-import java.util.Set;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.library.plantuml.PlantUmlArchCondition.Configurations.consideringOnlyDependenciesInAnyPackage;
 import static com.tngtech.archunit.library.plantuml.PlantUmlArchCondition.Configurations.consideringOnlyDependenciesInDiagram;
 import static com.tngtech.archunit.library.plantuml.PlantUmlArchCondition.adhereToPlantUmlDiagram;
 import static com.tngtech.archunit.thirdparty.com.google.common.io.Resources.getResource;
