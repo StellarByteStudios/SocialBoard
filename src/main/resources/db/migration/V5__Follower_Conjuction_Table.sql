12fc7dd1d801e93eb488015c09c2f8f92f838aec
StellarByteStudios <82361848+StellarByteStudios@users.noreply.github.com>
2022-03-06 09:59:07 +0100

    Conjunctiontabel for Follower
    Tabelle erstellt, welche die Verbindungen zwischen den Nutzerpaaren herstellt

---
+++ src/main/resources/db/migration/V5__Follower_Conjuction_Table.sql
@@ -0,0 +1,13 @@
+-- Erzeugen der Tabelle in der die Userdaten gespeichert werden --
+create table UserFollowerConjunction
+(
+    -- ID des Folgenden --
+	Follower_ID int,
+	-- ID des Gefolgten --
+	User_DTO int,
+	-- Macht die gemeinsame Verbindung zum Primärschlüssel --
+	primary key (Follower_Ref, User_DTO)
+    -- Macht die Fremdschlüsselverknüpfung --
+    constraint UserFollowerConjunction_Users_fk
+        foreign key (User_DTO) references Users (userID)
+);
